ecb66d05a27e60fa728018d5b922cd65bec046da
GITCDHW <pank36720@gmail.com>
2026-02-14 22:36:54 +0530

    update page.js

--- app/demos/clinic/page.js
+++ app/demos/clinic/page.js
@@ -60,7 +60,7 @@ export default function MedicalCenterPage() {
           </div>
           <div className="relative">
             <img 
-              src="https://images.unsplash.com/photo-1586773860418-d3b97998c63a?auto=format&fit=crop&q=80&w=800" 
+              src="https://plus.unsplash.com/premium_photo-1682129906083-e802555764bf?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
               alt="Medical Center Interior" 
               className="rounded-2xl shadow-2xl border-8 border-slate-50"
             />
